8eb952a38def79f9375db9acb6b98757dd63c0a4
Artur Rosiek <archie.rosiek@gmail.com>
2022-10-21 21:00:19 +0200

    Drugie zadanie mogące pojawić się podczas rozmowy o pracę - Największa i najmnajmniejsza liczba z listy

--- [Praktyka] Przykładowe programy na rozmowę rekrutacyjną/Największa i najmniejsza liczba/main.py
+++ [Praktyka] Przykładowe programy na rozmowę rekrutacyjną/Największa i najmniejsza liczba/main.py
@@ -1,4 +1,27 @@
 # Zadanie jest następujące. Przy użyciu języka Python, należy znaleźć najmniejszą oraz największa liczbę na liście.
+sampleList = [1, 4, -4, 7, 8, 3]
 
-sampleList = [1, 4, -4, 7]
+def lowestNumber(listOfNumbers):
+    lenght = len(listOfNumbers)
+    highestValue = listOfNumbers[0]
+    for i in range(lenght):
+        if highestValue <= listOfNumbers[i]:
+            highestValue = listOfNumbers[i]
+            continue
+        else:
+            continue
+    print("Highest number in the list:", highestValue)
 
+def highestNumber(listOfNumbers):
+    lenght = len(listOfNumbers)
+    lowestValue = listOfNumbers[0]
+    for i in range(lenght):
+        if lowestValue >= listOfNumbers[i]:
+            lowestValue = listOfNumbers[i]
+            continue
+        else:
+            continue
+    print("Lowest number in the list:", lowestValue)
+
+lowestNumber(sampleList)
+highestNumber(sampleList)
