b9ad568819b4d3ad401e08a6f653986f3449d251
Artur Rosiek <archie.rosiek@gmail.com>
2023-04-17 21:13:08 +0200

    Dodano kolejną lekcję do repo

--- [Kurs] Strefa kursów - Fundamenty programowania w Python/02_4 Listy/main.py
+++ [Kurs] Strefa kursów - Fundamenty programowania w Python/02_4 Listy/main.py
@@ -1,1 +1,19 @@
-x = '123'
+# x = '123'
+# y = x.replace('2', '4')
+# x[1] = '4'
+# print(x, y)
+# x = ['1', 2, [3]]
+# print(x, type(x))
+# for obiekt in x:
+#     print(obiekt, type(obiekt))
+# x: list[int] = [1, 2, 3]
+# x[2] = 4
+# print(x, type(x))
+# x[0] = 'jeden'
+# print((x, type(x)))
+# x = [1] + [2, 3]
+# x.insert(len(x), 9)
+# x.append(999)
+# print(x)
+x = ['Pluton jest planetą!'] * 5
+print(x)
