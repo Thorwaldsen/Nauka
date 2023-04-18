ca690b8055d515b851ae446ab723a5742e9030f5
Artur Rosiek <archie.rosiek@gmail.com>
2023-04-18 16:46:06 +0200

    Dodano nowy moduł - krotki

---
+++ [Kurs] Strefa kursów - Fundamenty programowania w Python/02_5 Krotki/main.py
@@ -0,0 +1,20 @@
+# x = ()
+# print(x, type(x))
+# y = (1, 2)
+# print(y, type(y))
+# z = 1, "a", "Python", 2.0, [1, 2, 3], (4, 5, 6)
+# print(z, type(z))
+# strings = z[1:3]
+# print(strings, type(strings))
+# x = 'a', 'b', 'c'
+# idx = x.index('c')
+# print(x, type(x))
+# cnt = x.count('a')
+# print("Indeks 'c' :", idx, type(idx))
+# print("Wystąpienia 'a': ", cnt, type(cnt))
+x = 1, 2, 3
+y = x + x
+z = x * 3
+print("x:", x)
+print("x + x:", y)
+print("x * 3: ", z)
