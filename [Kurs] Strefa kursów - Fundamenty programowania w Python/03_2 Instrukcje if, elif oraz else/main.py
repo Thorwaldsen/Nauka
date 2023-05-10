4f59b744d15faf7ee7ef30a7e5481bf21aa919b1
Artur Rosiek <archie.rosiek@gmail.com>
2023-05-10 21:16:19 +0200

    Dodano nową lekcję - if, elif, else

---
+++ [Kurs] Strefa kursów - Fundamenty programowania w Python/03_2 Instrukcje if, elif oraz else/main.py
@@ -0,0 +1,18 @@
+print("Pierwsza część kodu")
+x, y, z = 9, 2, 3
+if x < y and z == 3:
+    print("Druga część kodu")
+elif x > z:
+    print("Blok kodu zagnieżdżony w ELIF")
+else:
+    print("Dodana część kodu")
+print("Trzecia część kodu")
+
+#if wyrażenie -> bool:
+#    zagnieżdżony blok instrukcji sterującej
+#elif wyrażenie2 -> bool:
+#    zagnieżdzony blok kodu
+#elif wyrażenieN -> bool:
+#    zagnieżdzony blok kodu
+#else:
+#    blok wykonywany wtedy, gdy wyrażenie jest False
