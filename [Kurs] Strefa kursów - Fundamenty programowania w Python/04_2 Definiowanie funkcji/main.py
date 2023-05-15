32966a6a29371a305f17de014241440367c76a2a
Artur Rosiek <archie.rosiek@gmail.com>
2023-05-15 21:26:15 +0200

    Dodano dwie lekcje - funkcje i parametry funkcji

---
+++ [Kurs] Strefa kursów - Fundamenty programowania w Python/04_2 Definiowanie funkcji/main.py
@@ -0,0 +1,39 @@
+"""
+def(definiuję) funkcja()(Funkcja o nazwie 'Funkcja', która...):
+    ...wykonuje instrukcje i wyrażenia tutaj opisane
+"""
+
+# def interakcja_z_handlarzem():
+#     print("Handlarz: Witaj wędrowcze")
+#     print("Handlarz: Czy masz może ochotę pohandlować?")
+#     while True:
+#         odpowiedz = input("Ty: ")
+#         if odpowiedz == "Tak":
+#             print("Handlarz: To świetnie. Nie zawiedziesz się")
+#             print("*Handlarz pokazuje Ci swoje towary*")
+#             break
+#         elif odpowiedz == "Nie":
+#             print("Handlarz: Może następnym razem...")
+#             print("*Handlarz uśmiecha się krzywo*")
+#             break
+#         else:
+#             print("Przykro mi, ale nie zrozumiałem")
+#             continue
+
+
+# x = interakcja_z_handlarzem
+
+# print(x, type(x))
+
+# x()
+
+x = 10
+y = 12
+if x > y:
+    def function():
+        print("x jest większe od y")
+else:
+    def function():
+        print("x jest mniejsze od y")
+
+function()
---
+++ [Kurs] Strefa kursów - Fundamenty programowania w Python/04_3 Parametry funkcji (Parametry i argumenty funkcji)/main.py
@@ -0,0 +1,10 @@
+def iloczyn(a, b, c, d=5):
+    wynik = a * b + c * d
+    print("a:", a)
+    print("b:", b)
+    print("c:", c)
+    print("d:", d)
+    print("Wynik: ", wynik)
+
+
+iloczyn(2, c=3, b=4)
