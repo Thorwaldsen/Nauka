946a94bb95d847f10020ca917b9c0741fbd2475f
Artur Rosiek <archie.rosiek@gmail.com>
2023-05-22 19:58:07 +0200

    Dodano dwa moduły - podstawy pisania klas

---
+++ [Kurs] Strefa kursów - Fundamenty programowania w Python/05_2 Podstawy tworzenia klas/main.py
@@ -0,0 +1,9 @@
+class Klasa:
+    liczba_uczniow = 30
+
+
+instancja_klasy = Klasa()
+print(type(instancja_klasy))
+
+liczba_uczniow = instancja_klasy.liczba_uczniow
+print((liczba_uczniow, type(liczba_uczniow)))
