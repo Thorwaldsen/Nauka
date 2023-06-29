fc66daf4ef383155446fe79add067418d8a12e1c
Artur Rosiek <archie.rosiek@gmail.com>
2023-06-29 21:12:36 +0200

    Dodano jeden moduł - ciąg fibbonacciego

---
+++ [Kurs] Python średniozaawansowany - Adam Djellouli/09 Generator liczb Fibonacciego/main.py
@@ -0,0 +1,38 @@
+# uzyskujemy n pierwszych wyrazów ciągu Fibbonacciego za pomocą generatora:
+def genFib(n):
+    a, b = 0, 1
+    for i in range(n + 1):
+        yield a
+        a, b = b, a + b
+
+
+print(list(genFib(5)))
+
+
+# uzyskujemy n pierwszych wyrazów ciągu Fibbonacciego za pomocą iteratora:
+class ifFib():
+
+    def __init__(self, n):
+        self.a = 0
+        self.b = 1
+        self.i = 0
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i > self.n:
+            raise StopIteration
+        self.i += 1
+        x = self.a
+        self.a, self.b = self.b, self.a + self.b
+        return x
+
+
+lista = []
+
+for x in ifFib(5):
+    lista.append(x)
+
+print(lista)
