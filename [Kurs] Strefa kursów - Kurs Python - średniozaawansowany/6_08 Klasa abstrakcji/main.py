43d6986388fdd0575c84044e1a1bf88ca6e67764
Artur Rosiek <archie.rosiek@gmail.com>
2023-08-03 19:22:36 +0200

    Dodano lekcję - klasa abstrakcji

---
+++ [Kurs] Strefa kursów - Kurs Python - średniozaawansowany/6_08 Klasa abstrakcji/main.py
@@ -0,0 +1,13 @@
+import Classes as Animal
+
+boy = Animal.Boy(4, "Miłosz", "Koska")
+
+boy.print_information()
+boy.give_voice()
+
+boy.go_to()
+
+# animal = Animal.Animal()
+
+# animal.print_information()
+# animal.voice()
