22c7bb9bdf30ff2cf4b35dcfc4706908c276a9d0
Artur Rosiek <archie.rosiek@gmail.com>
2023-08-12 16:26:05 +0200

    Dodano rysowanie tła planszy

--- [Kurs] Strefa kursów - Kurs Python - średniozaawansowany/6_11 Obiektowo - Tic Tac Toe/main.py
+++ [Kurs] Strefa kursów - Kurs Python - średniozaawansowany/6_11 Obiektowo - Tic Tac Toe/main.py
@@ -8,6 +8,26 @@ textureX = sf.Texture.from_file("./x.png")
 textureO = sf.Texture.from_file("./o.png")
 textureS = sf.Texture.from_file("./skull.png")
 
+array_x = []
+for iteration_x in range(9):
+    sprite = sf.Sprite(textureX)
+    array_x.append(sprite)
+
+array_o = []
+for iteration_o in range(9):
+    sprite = sf.Sprite(textureO)
+    array_o.append(sprite)
+
+array_s = []
+for iteration_s in range(9):
+    sprite = sf.Sprite(textureS)
+    array_s.append(sprite)
+
+# Board
+objectBoard = Board("X")
+
+mousePosition = sf.Vector2(-1, -1)
+
 while window.is_open:
     window.clear(sf.Color.WHITE)
 
@@ -17,6 +37,8 @@ while window.is_open:
     # exit
     if sf.Keyboard.is_key_pressed(sf.Keyboard.ESCAPE):
         window.colse()
+
+    board = objectBoard.return_board()
 
     # body game
     rectangle_one = sf.RectangleShape()
@@ -47,5 +69,21 @@ while window.is_open:
     window.draw(rectangle_two)
     window.draw(rectangle_three)
     window.draw(rectangle_four)
-    window.display()
 
+    draw_array = []
+    for x_array in range(3):
+        for y_array in range(3):
+            if board[x_array][y_array] == ".":
+                array_s[3 * x_array + y_array].position = sf.Vector2(200 * x_array, 200 * y_array)
+                draw_array.append(array_s[3 * x_array + y_array])
+            elif board[x_array][y_array] == "X":
+                array_x[3 * x_array + y_array].position = sf.Vector2(200 * x_array, 200 * y_array)
+                draw_array.append(array_x[3 * x_array + y_array])
+            else:
+                array_o[3 * x_array + y_array].position = sf.Vector2(200 * x_array, 200 * y_array)
+                draw_array.append(array_o[3 * x_array + y_array])
+
+    for element_to_draw in range(9):
+        window.draw(draw_array[element_to_draw])
+
+    window.display()
